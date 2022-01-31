8df835634c0edf168076636120b5dbcbd22a9a5b
Hatef OTROSHI <hatef.otroshi@idiap.ch>
2022-01-31 16:23:48 +0100

    + [PUT-palm] bash files

---
+++ PUT/PUT-palm/execute_fvr.sh
@@ -0,0 +1,36 @@
+root_dir=$(pwd)
+
+for model in {wld,rlt,mc}
+do
+    # AE
+    cd $root_dir
+    cd models
+    cd $model
+    cd AE/alpha_1/emb_100/verify
+    verify.py $model-AE-BHsh-100.py -vvv
+    verify.py $model-AE-BHsh-100-stolen.py -vvv
+
+
+    # Baseline
+    cd $root_dir
+    cd models
+    cd $model
+    cd Baseline
+    verify.py $model.py -vvv
+    verify.py $model-BHsh-100.py -vvv
+    verify.py $model-BHsh-100-stolen.py -vvv
+    verify.py $model-BHsh-500.py -vvv
+    verify.py $model-BHsh-500-stolen.py -vvv
+    verify.py $model-BHsh-1000.py -vvv
+    verify.py $model-BHsh-1000-stolen.py -vvv
+done
+
+
+# img
+alpha=1
+cd $root_dir
+cd models
+cd img
+cd AE/alpha_$alpha/emb_100/verify
+verify.py $model-AE-BHsh-100.py -vvv
+verify.py $model-AE-BHsh-100-stolen.py -vvv
